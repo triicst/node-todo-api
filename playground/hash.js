414b532974e1a890deb55eb5afa54f9fbce136dd
triicst <tri.pham.hpc@gmail.com>
2018-09-06 14:01:08 +0700

    hasing password

--- playground/hash.js
+++ playground/hash.js
@@ -1,12 +1,23 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
-var data = {
-    id: 10
-};
-var token = jwt.sign(data,'123abc');
-console.log(token);
-var decode = jwt.verify(token,'123abc');
-console.log(decode);
+const bcrypt = require('bcryptjs');
+var password = 'abc123!';
+bcrypt.genSalt(10,(err,salt) =>{
+    bcrypt.hash(password,salt,(err, hash)=>{
+        console.log(hash);
+    });
+});
+var hashPassword = '$2a$10$qXMIo6jouo5/f1kZNA8J.eYW9W2V0e.onW.Yw/xfKC9qZ2yc41VOS'
+bcrypt.compare(password,hashPassword, (err,res) =>{
+    console.log(res);
+});
+// var data = {
+//     id: 10
+// };
+// var token = jwt.sign(data,'123abc');
+// console.log(token);
+// var decode = jwt.verify(token,'123abc');
+// console.log(decode);
 // var message ='My name Tri Pham';
 // var hash = SHA256(message).toString();
 // console.log(`Message: ${message}`);
