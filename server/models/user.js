b6e48b15e384bebabf6e732dd48b1b2a06b91c16
triicst <tri.pham.hpc@gmail.com>
2018-09-06 15:07:37 +0700

    remove token

--- server/models/user.js
+++ server/models/user.js
@@ -47,6 +47,16 @@ UserSchema.methods.generateAuthToken = function () {
       return token;
     });
   };
+UserSchema.methods.removeToken = function (token) {
+    var user = this;
+    return user.update({
+            $pull: {
+                tokens: {
+                    token:token
+            }
+        }
+    });
+};
 UserSchema.statics.findByToken = function (token) {
     var User = this;
     var decode;
@@ -76,5 +86,22 @@ UserSchema.pre('save', function(next){
         next();
     }
 });
+UserSchema.statics.findByCredentials = function (email,password) {
+    var User = this;
+    return User.findOne({email}).then((user) =>{
+        if (!user){
+            return Promise.reject();
+        }
+        return new Promise((resolve,reject) =>{
+            bcrypt.compare(password, user.password, (err,res) =>{
+                if (res){
+                    resolve(user);
+                } else{
+                    reject();
+                }
+            });
+        });
+    });
+};
 var User = mongoose.model('User',UserSchema);
 module.exports = {User}
--- server/server.js
+++ server/server.js
@@ -105,6 +105,24 @@ app.post('/users',(req,res) =>{
 app.get('/users/me',authenticator, (req,res) =>{
     res.send(req.user);
 });
+app.post('/users/login',(req,res) =>{
+    var body = _.pick(req.body,['email','password']);
+    User.findByCredentials(body.email,body.password).then((user) =>{
+        return user.generateAuthToken().then((token) =>{
+            res.header('x-auth',token).send(user);
+        });
+        //res.send(user);
+    }).catch((err) =>{
+        res.status(400).send(err);
+    }); 
+});
+app.delete('/users/me/token',authenticator, (req,res) =>{
+    req.user.removeToken(req.token).then(() =>{
+        res.status(200).send();
+    }).catch(() =>{
+        res.status(400).send()
+    });
+});
 app.listen(port,() =>{
     console.log(`Started on port ${port}`);
 });
